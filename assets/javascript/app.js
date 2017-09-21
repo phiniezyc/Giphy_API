dcd7f9598379d07ce2a69ad1feace9cd9e62eb9e
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-21 00:33:24 -0400

    search button pulls in and displays gifs to the page, however display needs to be fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,53 @@
 
-var userSearchInput = $("#userSearchInput").val().trim();
+//Input to page
+//Create an array of search terms
+//Use for loop to create buttons that will search for new gifs and put to page
+//Use user search term to add to array do same thing 
+//Input to page for each new search (using a for loop)
+
+var searchArray = [];
+
+    
+
+$("#submitButton").click(function () { 
+    var userSearchInput = $("#userSearchInput").val().trim();
+    console.log("button works!")
     console.log(userSearchInput);
 
-//var searchArray = [];
+    //Array 
+    searchArray.push(userSearchInput);
+    console.log(searchArray);
 
 
-
-
-
-
-
-//var searchButton = $("#submitButton").click(function() { 
-    //console.log(userSearchInput);
-    
-//});
-
-console.log("test");
-
-var queryURL = "https:api.giphy.com/v1/gifs/search?api_key=83e0322b808940ff8c755f9e308c5537";
-var searchTerm = "&q=Costas";
+var queryURL = "https:api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=";
+var searchTerm = userSearchInput;
 
 var gifParameter = "&limit=10&offset=0&rating=G&lang=en";
 
     $.ajax({
       url: queryURL + searchTerm + gifParameter,
+      //url: "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=Costas&limit=25&offset=0&rating=G&lang=en", 
       method: 'GET'
     }).done(function(response) {
       console.log(response);
+
+      //need to save gif to returned to a variable and then convert to image element. Then put that in append to page
+      var imgUrl = response.data[0].images.fixed_height.url;
+      var gifs = $("<img>");
+      gifs.attr("src", imgUrl);
+      gifs.attr("alt", "cat image");
+      
+      
+      $("#gifDisplaySection").append(gifs);
+      $("#gifDisplaySection").append("Rating: ", response.data[0].rating);
+      
     });
+    
+});
+
+
+
+
 
 
 
