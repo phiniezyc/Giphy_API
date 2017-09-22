4e26eff2ab1018cc8a669de19c01afc068dd584c
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-22 01:12:41 -0400

    post revert update--everything appears to be working as before

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,5 @@
 
-// need to get user input to create a button by adding to existing array.
-// want to make button produce user input search
-// put all the code below inside those buttons.
-
+//Input to page
 //Create an array of search terms
 //Use for loop to create buttons that will search for new gifs and put to page
 //Use user search term to add to array do same thing 
@@ -15,31 +12,10 @@
 var gifsToDisplay = ["Bob Costas", "Beyonce", "Wine"];
 
 
-for (var i = 0; i < gifsToDisplay.length; i++) {
-    // Then dynamicaly generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var buttonCreation = $("<button>");
-    // Adding a class of movie to our button
-    buttonCreation.addClass("displayButtons");
-    // Adding a data-attribute
-    buttonCreation.attr("data-name", gifsToDisplay[i]);
-    // Providing the initial button text
-    buttonCreation.text(gifsToDisplay[i]);
-    // Adding the button to the buttons-view div
-    $("#buttonSection").append(buttonCreation);
-}
-
-$(".displayButtons").click(function() { 
-    console.log("Buttons work2!");
-    getGifs();
-      
-      
-    
-});
     
 //Functions =========================================================================
 
-function getGifs(){
+
 
 
 $("#submitButton").click(function () { 
@@ -105,21 +81,21 @@ var gifParameter = "&limit=10&offset=0&rating=G&lang=en";
       });
 
       //need to save gif to returned to a variable and then convert to image element. Then put that in append to page
-      /*var imgUrl = response.data[0].images.fixed_height.url;
+      var imgUrl = response.data[0].images.fixed_height.url;
       var gifs = $("<img>");
       gifs.attr("src", imgUrl);
       gifs.attr("alt", "cat image");
       
       
       $("#gifDisplaySection").append(gifs);
-      $("#gifDisplaySection").append("Rating: ", response.data[0].rating);*/
+      $("#gifDisplaySection").append("Rating: ", response.data[0].rating);
       
     });
     
 });
 
 
-}
+
 
 
 
